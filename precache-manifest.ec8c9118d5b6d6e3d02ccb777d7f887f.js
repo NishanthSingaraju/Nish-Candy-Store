2d6b8291b1ceb38fa6b642e3bc3b63c88621d039
Nishanth Singaraju <nishanth.singaraju@gmail.com>
2019-05-14 21:57:41 -0400

    Updates

---
+++ precache-manifest.ec8c9118d5b6d6e3d02ccb777d7f887f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0d69bbcefedb5d085ca1",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/static/css/main.3880e4c6.chunk.css"
+  },
+  {
+    "revision": "0d69bbcefedb5d085ca1",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/static/js/main.0d69bbce.chunk.js"
+  },
+  {
+    "revision": "9c08b152384ac49cdf31",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/static/css/1.1bf8fa84.chunk.css"
+  },
+  {
+    "revision": "9c08b152384ac49cdf31",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/static/js/1.9c08b152.chunk.js"
+  },
+  {
+    "revision": "251e94981357ad7e0a5f",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/static/js/runtime~main.251e9498.js"
+  },
+  {
+    "revision": "9f093ed0f5f8657b59c687c636c02400",
+    "url": "/NishanthSingaraju/Nish-Candy-Store.git/index.html"
+  }
+];
